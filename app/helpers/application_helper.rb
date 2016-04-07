86e4e86bfd7ba86ee5244f37aad6daa1245c3acf
zypz1990 <zypz1990@163.com>
2016-04-07 15:25:16 +0800

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(front_title='')
+		behind_title="Ruby on Rails Tutorial Sample App"
+		if front_title.empty?
+			behind_title
+		else
+			front_title+ " | " +behind_title
+		end
+	end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+	def setup
+		@part_title="Ruby on Rails Tutorial Sample App"
+	end
+	
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title","Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title","Home | #{@part_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title","Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title","Help | #{@part_title}"
   end
 
   test "shout get about" do
   	get :about
   	assert_response :success
-  	assert_select "title","About | Ruby on Rails Tutorial Sample App"
+  	assert_select "title","About | #{@part_title}"
   end
 end
